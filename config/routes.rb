b8a3cbf976789044c7f6dfecd4b61b6c8702ccfa
Paul <gttahi@gmail.com>
2017-03-07 19:34:37 +0300

    Update hello-app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#helloworld'
+  root 'application#I am a tech-entrepreneur with a vision to setup a start-up ecosystem in 2017'
   
 end
 	
